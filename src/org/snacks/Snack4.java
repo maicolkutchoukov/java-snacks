265eccb4f310352be49b71ffa37fafc1cd620d46
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-16 14:38:29 +0200

    Complete snack4

---
+++ src/org/snacks/Snack4.java
@@ -0,0 +1,27 @@
+package org.snacks;
+
+import java.util.Scanner;
+
+/*
+*Snack4
+Data in input una stringa verificare se è palindroma
+* ITOPINONAVEVANONIPOTI *
+**/
+public class Snack4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci una stringa:");
+        String str = scanner.nextLine();
+        String str2 = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.println(i);
+            str2 += str.charAt(i);
+        }
+        if (str.equals(str2)){
+            System.out.println("La parola inserita è palindroma");
+        } else {
+            System.out.println("La parola inserita non è palindroma");
+        }
+        System.out.println(str2);
+    }
+}

121be129b3edd55f7ea92d439be6d554df8975df
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-16 14:01:28 +0200

    Complete snack1

--- src/org/snacks/Snack1.java
+++ src/org/snacks/Snack1.java
@@ -1,2 +1,20 @@
-package org.snacks;public class Snack1 {
+package org.snacks;
+
+import java.util.Scanner;
+
+/*
+Snack1
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+*/
+public class Snack1 {
+    public static void main(String[] args) {
+        System.out.print("Inserisci un numero:");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n % 2 == 0){
+            System.out.println(n);
+        } else {
+            System.out.println(n+1);
+        }
+    }
 }

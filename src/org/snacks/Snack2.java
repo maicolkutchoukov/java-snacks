32acf2deb8a25f2d2a76bc00d4afd0c090d9b3b7
maicolkutchoukov <maicol.j.95@gmail.com>
2024-05-16 14:20:59 +0200

    Complete snack2

---
+++ src/org/snacks/Snack2.java
@@ -0,0 +1,26 @@
+package org.snacks;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+* Snack2
+Generatore di “nomi cognomi” casuali:
+il Grande Gatsby ha una lista di nomi e una lista di cognomi,
+e da queste vuole mostrare a video una falsa lista di invitati con nome e cognome.
+ */
+public class Snack2 {
+    public static void main(String[] args) {
+        String[] names = {"Francesco", "Alessio", "Luigi", "Marco", "Leonardo", "Andrea", "Giacomo", "Fabrizio", "Tommaso", "Camilla"};
+        String[] surnames = {"Rossi", "Bianchi", "Verdi", "Giorgi", "Arancioni", "Viola", "Grigio", "Ferri", "Dari", "Sonno"};
+        Random randomGenerator = new Random();
+        String[] persons = new String[10];
+        for (int i = 0; i < persons.length; i++) {
+            int randomNumOne = randomGenerator.nextInt(10);
+            int randomNumTwo = randomGenerator.nextInt(10);
+            persons[i] = names[randomNumOne] + ' ' + surnames[randomNumTwo];
+        }
+        System.out.println(Arrays.toString(persons));
+
+    }
+}
